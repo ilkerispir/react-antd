92639e99f745f8a693dc717ed51629d2ed08c85c
ilkerispir <ilker.ispir@alo-tech.com>
2020-11-28 17:38:00 +0300

    feat: created grid

--- src/views/home.js
+++ src/views/home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-import AppHero from '../components/home/hero'
+import AppHero from '../components/home/hero';
+import AppAbout from '../components/home/about';
 
 function AppHome() {
     return (
         <div className="main">
             <AppHero />
+            <AppAbout />
         </div>
     );
 }
